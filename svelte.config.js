566eff07c7fb10bf56eebba550c727194f2c6ac1
Willem Vanhulle <willemvanhulle@inbiose.com>
2021-12-28 20:33:36 +0100

    init

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,8 +14,10 @@ const config = {
 	preprocess: [preprocess({}), mdsvex(mdsvexConfig)],
 
 	kit: {
-		adapter: adapter(),
-
+		adapter: adapter({pages: "docs"}),
+		paths: {
+					base: dev ? '' : '/wvhulle.github.io/',
+				},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
