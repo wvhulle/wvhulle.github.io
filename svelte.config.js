b45a70b8d82561e2bca191f9a795a90e80b2754c
Willem Vanhulle <willem.vanhulle@inbiose.com>
2023-05-22 22:25:35 +0200

    using another sveltekit yml

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 		adapter: adapter({ pages: 'build' }),
 
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '' : ''
+			base: process.env.NODE_ENV === 'production' ? '/wvhulle.github.io' : ''
 		}
 	}
 };
